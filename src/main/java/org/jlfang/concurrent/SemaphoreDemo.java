e7a78da27e2ba12768a75e08bc584addacf894ce
Fang Jialue <cris@Fangs-MacBook-Pro.local>
2020-01-26 20:29:57 +0800

    feat: add some demos

---
+++ src/main/java/org/jlfang/concurrent/SemaphoreDemo.java
@@ -0,0 +1,28 @@
+package org.jlfang.concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreDemo {
+    public static void main(String[] args) {
+        final int clientCount = 3;
+        final int totalRequestCount = 10;
+        Semaphore semaphore = new Semaphore(clientCount);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < totalRequestCount; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    Thread.sleep(1000);
+                    System.out.print(semaphore.availablePermits() + " ");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    semaphore.release();
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
